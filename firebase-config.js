15ada816274b8b367e1de5204353d8a8a62a2459
Vermavnish <166979960+Vermavnish@users.noreply.github.com>
2025-05-28 16:37:34 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDw5IKZIiMvlvS1L8ofg6cKKMtXrVeN6zQ",
+  authDomain: "avni-c6af6.firebaseapp.com",
+  databaseURL: "https://avni-c6af6-default-rtdb.firebaseio.com",
+  projectId: "avni-c6af6",
+  storageBucket: "avni-c6af6.appspot.com",
+  messagingSenderId: "158260273585",
+  appId: "1:158260273585:web:06f310e48a87f404815d90",
+  measurementId: "G-ZBNE463P71"
+};
+
+firebase.initializeApp(firebaseConfig);
